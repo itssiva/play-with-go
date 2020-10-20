4021ada959b6b8685a26f0707c2bdf3d7e1448b1
Siva Ponugoti <siva.ponugoti@jet.com>
2020-10-20 06:28:08 -0400

    create a module and use it in another module

--- hello.go
+++ hello.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+	"example.com/greetings"
+)
+
+// import "rsc.io/quote"
+
+// func main() {
+// 		fmt.Println(quote.Go());
+// 		fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+// }
 
 func main() {
-    fmt.Println(quote.Go());
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
